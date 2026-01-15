c3380e21efdb7ceb2b3aa9d72a3c47b22fbef9a8
GarlicToothpaste <adrianpdanao@gmail.com>
2026-01-15 21:24:38 +0800

    Initialize llm

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
-import streamlit as st
-import os
-from pathlib import Path
 from langchain_ollama import OllamaEmbeddings, ChatOllama
 from langchain_chroma import Chroma
-from langchain_community.document_loaders import PyPDFLoader, TextLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from pathlib import Path
+import streamlit as st
 
 # --- CONFIG ---
 WATCH_PATH = Path("./knowledge_base")
 DB_PATH = "./chroma_db"
+MODEL_NAME = "qwen3:4b"
 EMBED_MODEL = "nomic-embed-text:latest"
+
+WATCH_PATH.mkdir(exist_ok=True)
+
+@st.cache_resource
+def initialize_llm():
+    """Initializes the LLM"""
+    llm = ChatOllama(model=MODEL_NAME, temperature = 0.7)
+    
+    return llm
 
